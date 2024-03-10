2377c8c06895a4c2477e4539c6e7a25197364c4c
Reful4ent <dima2323fr3@gmail.com>
2024-03-10 16:55:37 +0700

    Did Create, Print and Fill methods for Matrix

---
+++ CompMath_Lab_2/Matrix.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompMath_Lab_2
+{
+    public static class Matrix
+    {
+
+
+
+        public static void CreateMatrix(int size, out int[,] mainMatrix, out int[,] ansMatrix)
+        {
+            mainMatrix = new int[size, size];
+            ansMatrix = new int[size, 1];
+            FillMatrix(ref mainMatrix,ref ansMatrix);
+        }
+
+        private static void FillMatrix(ref int[,] mainMatrix, ref int[,] ansMatrix)
+        {
+            //GetUpperBound(0) + 1;    // количество строк
+            //GetUpperBound(1) + 1;;        // количество столбцов
+            for (int i = 0; i < mainMatrix.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < mainMatrix.GetUpperBound(1) + 1; j++)
+                {
+                    mainMatrix[i,j] = Convert.ToInt32( Console.ReadLine());
+                }
+            }
+            for(int i = 0; i < mainMatrix.GetUpperBound(0) + 1; i++)
+                ansMatrix[i,0] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static void PrintMatrix(int[,] mainMatrix,int[,] ansMatrix)
+        {
+            for(int i=0; i< mainMatrix.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < mainMatrix.GetUpperBound(1) + 1; j++)
+                {
+                    Console.Write(mainMatrix[i, j] + " ");
+                }
+                Console.Write("\t" + ansMatrix[i, 0]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CompMath_Lab_2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+
+
+
+using CompMath_Lab_2;
+
+int[,] matrix;
+int[,] result;
+Matrix.CreateMatrix(2,out matrix,out result);
+Matrix.PrintMatrix(matrix,result);
